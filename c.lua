deacfd0e3ca66c3dd29cf4ff6fdaedcc184d101f
ame <grantsquires@disroot.org>
2023-10-24 23:51:50 -0500

    sha2xx

--- c.lua
+++ c.lua
@@ -2,4 +2,7 @@ require "llib"
 print(llib.crypto.md5("wowa"));
 print(llib.crypto.sha1("wowa"));
 print(llib.crypto.sha0("wowa"));
+print(llib.crypto.sha256("wowa"));
+print(llib.crypto.sha224("wowa"));
+
 
