c3d9a4d44c1c6930fac277d20c6ea48801e39a18
ame <grantsquires@disroot.org>
2023-11-03 19:16:30 -0500

    pearson & xxhash

--- c.lua
+++ c.lua
@@ -1,8 +1,10 @@
 require "llib"
-print(llib.crypto.md5("wowa"));
-print(llib.crypto.sha1("wowa"));
-print(llib.crypto.sha0("wowa"));
-print(llib.crypto.sha256("wowa"));
-print(llib.crypto.sha224("wowa"));
+print(llib.crypto.xxh64("nya",5))
+print(llib.crypto.xxh32("nya",5))
+--print(llib.crypto.md5("wowa"));
+--print(llib.crypto.sha1("wowa"));
+--print(llib.crypto.sha0("wowa"));
+--print(llib.crypto.sha256("wowa"));
+--print(llib.crypto.sha224("wowa"));
 
 
