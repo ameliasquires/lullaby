79e6627bf92bb2bfd5a103cc4006994309b48246
ame <grantsquires@disroot.org>
2023-12-07 16:01:43 -0600

    string stuff & closed file from reading

---
+++ src/sort.h
@@ -0,0 +1,20 @@
+#include "lua.h"
+#include "i_util.h"
+#include "i_common.h"
+
+//comparison sorts
+int l_quicksort(lua_State*);    //[double+int] -> arr[N] (greatest -> least)
+int l_mergesort(lua_State*);    //[double+int] -> arr[N] (greatest -> least) 
+int l_shellsort(lua_State*);    //[double+int] -> arr[N] (greatest -> least)
+int l_bubblesort(lua_State*);   //[double+int] -> arr[N] (greatest -> least)
+int l_heapsort(lua_State*);     //[double+int] -> arr[N] (greatest -> least)
+
+//non-comparison sorts
+  //good for large arrays filled with small values
+int l_countingsort(lua_State*); //[int] (arr[N] >= 0) -> arr[N] (least -> greatest)
+
+//esoteric sorts
+int l_miraclesort(lua_State*);  //[double+int] -> arr[-∞<=N<=∞] (greatest -> least)
+int l_stalinsort(lua_State*);   //[double+int] -> arr[?<=N] (greatest -> least)
+int l_slowsort(lua_State*);     //[double+int] -> arr[N] (greatest -> least)
+int l_bogosort(lua_State*);     //[double+int] -> arr[N] (greatest -> least)
--- t.lua
+++ t.lua
@@ -1,11 +1,20 @@
 require "llib"
 local a = llib.array
 
-local test = {5,"meow",3,2,2,1,8}
-test = a.reverse(test)
+--local test = {5,"meow",3,2,2,1,8}
+--test = a.reverse(test)
 
-for i=1,#test do
-  print(test[i])
+--for i=1,#test do
+--  print(test[i])
+--end
+
+--print(a.index(test,"meow"))
+local ww = a.to_char_array("meow         awa")--(a.split(llib.io.readfile("c.lua"),"\n"))
+for i=1,#ww do
+  print(i .. " " .. ww[i])
 end
 
-print(a.index(test,"meow"))
+llib.io.debug("meow")
+llib.io.log("hmm")
+llib.io.warn("nuh uh!")
+llib.io.error("void")
