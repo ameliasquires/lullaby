3c06c70cccd643109d9ee8dde2767460a68dcfb0
ame <grantsquires@disroot.org>
2023-12-07 11:52:39 -0600

    more string friendly

--- t.lua
+++ t.lua
@@ -1,32 +1,11 @@
 require "llib"
 local a = llib.array
 
-local tab = {}
-math.randomseed(os.time())
-for i=1,19 do
-  table.insert(tab,math.random(1,999));-- + math.random(1,999));
-end
---print("length of 99999 :\n")
---time = os.clock()
---local l1 = a.quicksort(tab)
---print("quick sort took "..os.clock()-time.."s")
---time = os.clock()
---local l2 = a.mergesort(tab)
---print("merge sort took "..os.clock()-time.."s")
---time = os.clock()
---local l3 = a.shellsort(tab)
---print("shell sort took "..os.clock()-time.."s")
---time = os.clock()
---local l4 = a.bubblesort(tab)
---print("bubble sort took "..os.clock()-time.."s")
---time = os.clock()
---local l5 = a.heapsort(tab)
---print("heap sort took "..os.clock()-time.."s")
---time = os.clock()
-local l6 = a.shellsort(tab)
+local test = {5,"meow",3,2,2,1,8}
+test = a.reverse(test)
 
-for l,i in pairs(l6) do
-  print(l6[l])
+for i=1,#test do
+  print(test[i])
 end
-print(a.sum(l6))
 
+print(a.index(test,"meow"))
