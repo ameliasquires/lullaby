8c0dc0f4baa231a359f9785da14b1df8e7a6c0ac
ame <grantsquires@disroot.org>
2023-12-25 15:10:55 -0600

    removed small tests

--- c.lua
+++
@@ -1,3 +0,0 @@
-require "llib"
-llib.config.set({file_chunksize = 2})
-print(llib.io.readfile("src/hash/md5.c"))
--- tt.lua
+++
@@ -1,9 +0,0 @@
-require "llib"
-
-local s = llib.array.split("0 5 3 10"," ")
-local b = {a=5}
-llib.config.set({max_depth=-1,collapse_all=1});
-llib.io.pprint({5,{"what"},{55,55,33,2,5,5,5},{{55,5,8},9},3,{5,{meow=3}},{meow={5,5}}})
---llib.io.pprint(llib.array.len(b))
-
-
