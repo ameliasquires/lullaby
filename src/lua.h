91957f73e6459a549926e614144101e16043e825
ame <amelia@amyy.cc>
2024-01-23 14:13:45 -0600

    almost working great, fix 35th con

--- src/lua.h
+++ src/lua.h
@@ -1,8 +1,21 @@
-#include <lua5.4/lua.h>
-#include <lua5.4/lualib.h>
-#include <lua5.4/lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 
-#define lua_objlen(L,i) lua_rawlen(L,(i))
-#define luaL_register(L, M, F) luaL_newlib(L, F);
+
+#if LUA_VERSION_NUM == 504
+    #define lreg(N, FN)\
+        lua_pushstring(L, N);\
+        luaL_register(L, NULL, FN);\
+        lua_settable(L, -3);
+
+    #define lua_objlen(L,i) lua_rawlen(L,(i))
+    #define luaL_register(L, M, F) luaL_newlib(L, F);
+#else
+    #define lreg(N, FN)\
+        lua_newtable(L);\
+        luaL_register(L, NULL, FN);\
+        lua_setfield(L, 2, N);
+#endif
 
 
