0e352b5254e53ff5410eaef0f2e6c973f7935ed7
ame <amelia@amyy.cc>
2024-05-09 10:31:26 -0500

    work on memory saftey

--- tests/h.lua
+++ tests/h.lua
@@ -1,14 +1,30 @@
 require "llib"
 
 llib.thread.lock(1)
+llib.thread.lock(2)
+llib.thread.unlock(2)
 
 local thread_a = llib.thread.async(function (res)    
     --os.execute("sleep 1")
     --print((_G.ll + "hi"):final())
-    
-    res(_G.llib.crypto.md5("meow"))
+    print("waiting..")
+    llib.thread.lock(1)
+    print("signal!")
+    res(_G.llib.crypto.md5())
     print("after")
 end)
 
-print(thread_a:await())
+os.execute("sleep 1")
+llib.thread.unlock(1)
+
+awa = thread_a:await()
+
+os.execute("sleep 1")
+for i=1,999 do
+print((awa + "hi"):final())
+end
+
+thread_a:clean()
+
+print("clean exit")
 
