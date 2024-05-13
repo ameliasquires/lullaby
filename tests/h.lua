897f5843d61cc2d827b719e27d83d7f9cff3a20e
ame <ameliasquires@disroot.org>
2024-05-12 21:00:33 -0500

    memopry stuff

--- tests/h.lua
+++ tests/h.lua
@@ -1,8 +1,8 @@
 require "llib"
 
 llib.thread.lock(1)
-llib.thread.lock(2)
-llib.thread.unlock(2)
+--llib.thread.lock(2)
+--llib.thread.unlock(2)
 
 local thread_a = llib.thread.async(function (res)    
     --os.execute("sleep 1")
@@ -10,21 +10,19 @@ local thread_a = llib.thread.async(function (res)
     print("waiting..")
     llib.thread.lock(1)
     print("signal!")
-    res(_G.llib.crypto.md5())
+    res()
     print("after")
 end)
 
 os.execute("sleep 1")
+print("unlock")
 llib.thread.unlock(1)
+
 
 awa = thread_a:await()
 
+--print((awa + "hi"):final())
 os.execute("sleep 1")
-for i=1,999 do
-print((awa + "hi"):final())
-end
-
 thread_a:clean()
 
 print("clean exit")
-
