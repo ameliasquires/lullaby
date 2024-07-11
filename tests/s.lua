5eb06c6392931f84a2cf254627de0a10467ef99c
ame <ameliasquires@disroot.org>
2024-07-11 12:33:10 -0500

    stress test

--- tests/s.lua
+++ tests/s.lua
@@ -3,11 +3,13 @@ local t = function (a) end
 require "llib"
 
 local a = llib.crypto.md5()
+--llib.io.pprint(_ENV.a)
 
-b = llib.thread.buffer(a)
+for i = 1,200 do
+  awa = function(a) end
+  llib.thread.async(awa):await()
+end
 
-b:mod(function(e) 
-  return e:update("meow")
-end)
-
-print(b:get():final())
+if a:final() ~= "d41d8cd98f00b204e9800998ecf8427e" then
+  print(a:final())
+end
