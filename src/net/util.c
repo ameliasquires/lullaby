e04afb81cddb5d1a48140f1c768deb70f359476a
ame <ameliasquires@disroot.org>
2025-08-05 15:07:31 -0500

    add length after check, duh

--- src/net/util.c
+++ src/net/util.c
@@ -16,16 +16,16 @@ int64_t recv_header(int client_fd, char** _buffer, char** header_eof){
       return -1;
     }
 
-    if((len += n) >= MAX_HEADER_SIZE){
-      return -2;
-    }
-
     // search the last 4 characters too if they exist
     // this could probably be changed to 3
-    int64_t start_len = len - 4 > 0 ? len - 4 : 0;
+    int64_t start_len = len - 4 > 0 ? len - 4 : len;
     int64_t search_end = len - 4 > 0 ? n + 4 : n;
     if((*header_eof = memmem(buffer + start_len, search_end, "\r\n\r\n", 4)) != NULL){
-      return len; 
+      return len + n; 
+    }
+
+    if((len += n) >= MAX_HEADER_SIZE){
+      return -2;
     }
 
     buffer = realloc(buffer, sizeof* buffer * (len + BUFFER_SIZE + 1));
