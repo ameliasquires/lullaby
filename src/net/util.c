5cf623fde7f1382c5328b0ddc37c18b31721cb9c
stilic <lateman-jpeg@outlook.fr>
2026-05-06 15:34:41 +0200

    Update util.c

--- src/net/util.c
+++ src/net/util.c
@@ -348,7 +348,7 @@ parray_t* route_match(parray_t* paths, char* request, larray_t** _params){
 
 map_t* mime_type = NULL;
 char* _mimetypes = "/etc/mime.types";
-uint64_t _mimetypes_len = 15;
+size_t _mimetypes_len = 15;
 
 void parse_mimetypes(){
   if(_mimetypes == NULL || _mimetypes_len == 0) return;
