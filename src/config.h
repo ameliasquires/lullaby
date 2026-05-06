a832c8e3c1abdd4bb267cb118ae85e94c8ce1c27
stilic <lateman-jpeg@outlook.fr>
2026-05-06 15:21:24 +0200

    fix?

--- src/config.h
+++ src/config.h
@@ -25,7 +25,7 @@ struct config {
     //location of table in lua registery
     int* c_table_idx;
     //length used for c_string or c_function
-    size_t* len;
+    uint64_t* len;
   } value;
 };
 
